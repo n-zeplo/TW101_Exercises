1866d83f1cf6ea3dee20c568bdeba001fd767f32
n-zeplo <nzeplowitz@gmail.com>
2015-04-01 13:37:30 -0400

    Add Main Method To Incrementer Test Driver

--- Chapter2/src/IncrementerTestDriver.java
+++ Chapter2/src/IncrementerTestDriver.java
@@ -2,4 +2,13 @@
  * Created by Nathan_Zeplowitz on 4/1/15.
  */
 public class IncrementerTestDriver {
+    public static void main(String[] args){
+        Incrementer incrementer = new Incrementer();
+
+        for(int i = 0; i < 5; i++){
+            incrementer.increment();
+        }
+
+        incrementer.total();
+    }
 }
