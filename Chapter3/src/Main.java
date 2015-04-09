d272688736ecea590c0bd97eb21fdc39fb83cd5d
n-zeplo <nzeplowitz@gmail.com>
2015-04-09 18:17:53 -0400

    Add Main Method to Create 5 Rectangle Objects

---
+++ Chapter3/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Nathan_Zeplowitz on 4/9/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double rectanglesTotalArea = 0;
+
+        Rectangle [] rectangles = new Rectangle[5];
+
+        for (int i = 0; i < 5; i++){
+            rectangles[i] = new Rectangle();
+        }
+
+    }
+}
