83833454e1c823e44d082f74494d95a5ae010f58
n-zeplo <nzeplowitz@gmail.com>
2015-04-09 21:16:57 -0400

    Add Total Perimeter and Area Calculation to Main

--- Chapter4/src/Main.java
+++ Chapter4/src/Main.java
@@ -4,18 +4,20 @@
 public class Main {
     public static void main(String[] args) {
         double rectanglesTotalArea = 0;
+        double rectanglesTotalPerimeter = 0;
 
         Rectangle [] rectangles = new Rectangle[5];
 
         for (int i = 0; i < 5; i++){
-            rectangles[i] = new Rectangle();
+            rectangles[i] = new Rectangle(1.0, 2.0);
         }
 
         for (int i = 0; i < rectangles.length; i++){
             rectanglesTotalArea += rectangles[i].area();
+            rectanglesTotalPerimeter += rectangles[i].perimeter();
         }
 
-        double rectanglesAverageArea = rectanglesTotalArea / rectangles.length;
-        System.out.println("Total Area is " + rectanglesAverageArea);
+        System.out.println("Rectangles' total area is " + rectanglesTotalArea);
+        System.out.println("Rectangles' total perimeter is " + rectanglesTotalPerimeter);
     }
 }
