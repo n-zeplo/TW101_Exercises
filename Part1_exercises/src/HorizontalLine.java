2dac2e017002a238800e1a31eb1efd2a2914b8a4
n-zeplo <nzeplowitz@gmail.com>
2015-04-11 12:19:47 -0400

    Add Second Exercise Horizontal Line

---
+++ Part1_exercises/src/HorizontalLine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nathan_Zeplowitz on 4/11/15.
+ */
+public class HorizontalLine {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++)
+            System.out.print("*");
+    }
+
+
+}
