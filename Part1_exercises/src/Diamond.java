7d1522887741975231cc0bdcc93b981645727f77
n-zeplo <nzeplowitz@gmail.com>
2015-04-11 12:47:15 -0400

    Add Solution To Diamond Exercise

---
+++ Part1_exercises/src/Diamond.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nathan_Zeplowitz on 4/11/15.
+ */
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Diamond diamond = new Diamond();
+
+        System.out.println("Enter a number");
+        int n = in.nextInt();
+        diamond.printUpperHalf(n);
+        diamond.printLowerHalf(n);
+
+    }
+
+    private void printUpperHalf(int n){
+        for (int row = 1; row <= n; row++) {
+            String line = "";
+            for (int spaces = 0; spaces < n - row; spaces++)
+                line += " ";
+            for (int stars = 1; stars < row * 2; stars++)
+                line += "*";
+            System.out.println(line);
+        }
+    }
+
+    private void printLowerHalf(int n){
+        for (int row = n - 1; row >= 1; row--) {
+            String line = "";
+            for (int spaces = 0; spaces < n - row; spaces++)
+                line += " ";
+            for (int stars = 1; stars < row * 2; stars++)
+                line += "*";
+            System.out.println(line);
+        }
+    }
+
+}
