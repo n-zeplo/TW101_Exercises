f5037c5045be24f3c525c4612f1a5f079d5232f1
n-zeplo <nzeplowitz@gmail.com>
2015-04-13 18:09:24 -0400

    Add Getters and Setters for Rectangle Class

--- Chapter4/src/Rectangle.java
+++ Chapter4/src/Rectangle.java
@@ -5,16 +5,33 @@ public class Rectangle {
     private double height = 1.0;
     private double width = 2.0;
 
+
     public Rectangle(double height, double width) {
-        this.height = height;
-        this.width = width;
+        setHeight(height);
+        setWidth(width);
     }
 
     public double area(){
-        return height * width;
+        return getHeight() * getWidth();
     }
 
     public double perimeter(){
-        return (2 * height) + (2 * width);
+        return (2 * getHeight()) + (2 * getWidth());
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
     }
 }
