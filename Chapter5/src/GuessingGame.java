4ee88f65d78105f041aa7139de3aec63b5224381
n-zeplo <nzeplowitz@gmail.com>
2015-04-14 15:15:26 -0400

    Add Main Game With Checks for User Guesses

--- Chapter5/src/GuessingGame.java
+++ Chapter5/src/GuessingGame.java
@@ -2,4 +2,24 @@
  * Created by Nathan_Zeplowitz on 4/14/15.
  */
 public class GuessingGame {
+    public static void main(String[] args) {
+        RandomNumber randomNumber = new RandomNumber();
+        GameHelper helper = new GameHelper();
+        boolean guessCorrect = false;
+
+        while(!guessCorrect){
+            String userGuess = helper.getUserInput("Guess a Number Between 1 and 100:");
+            String result = randomNumber.checkGuess(userGuess);
+
+            if (result.equals("Correct")) {
+                guessCorrect = true;
+                System.out.println("Your guess was correct. You Win!");
+                break;
+            }
+
+            System.out.println(result);
+
+        }
+    }
+
 }
