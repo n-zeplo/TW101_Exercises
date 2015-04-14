07ef6cadb5e9789be67a2b262e8802429c626061
n-zeplo <nzeplowitz@gmail.com>
2015-04-14 15:05:21 -0400

    Add RandomNumber Class to Generate RandomNumber

--- Chapter5/src/RandomNumber.java
+++ Chapter5/src/RandomNumber.java
@@ -2,4 +2,13 @@
  * Created by Nathan_Zeplowitz on 4/14/15.
  */
 public class RandomNumber {
+    private int number;
+
+    public RandomNumber(){
+        this.number = (int) (Math.random() * 100);
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
