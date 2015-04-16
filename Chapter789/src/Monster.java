41a73c3ee608a725dae0da545179cb8fc052b370
n-zeplo <nzeplowitz@gmail.com>
2015-04-15 20:03:21 -0400

    Refactor Out Unnecessary Public Abstract Keywords

--- Chapter789/src/Monster.java
+++ Chapter789/src/Monster.java
@@ -2,7 +2,7 @@
  * Created by Nathan_Zeplowitz on 4/14/15.
  */
 interface Monster {
-    public abstract void takeDamage(int amount);
-    public abstract String name();
-    public abstract int currentHitpoints();
+    void takeDamage(int amount);
+    String name();
+    int currentHitpoints();
 }
